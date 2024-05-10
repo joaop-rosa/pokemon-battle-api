31515f91046e9e8f73c14590f647e9225791dcf2
joao.rosa <joao.rosa@cwi.com.br>
2024-05-10 16:45:04 -0300

    battle invite

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { createServer } from "node:http"
 import { Server } from "socket.io"
+import crypto from "crypto"
 
 const port = 3001
 const app = express()
@@ -12,9 +13,14 @@ const io = new Server(server, {
 })
 
 let connectedUsers = []
+let challengesList = []
+
+function getUserById(socketId) {
+  return connectedUsers.find((u) => socketId === u.socketId)
+}
 
 function addUser(user) {
-  if (connectedUsers.find((u) => user.socketId === u.socketId)) return
+  if (getUserById(user.socketId)) return
 
   if (user.socketId && user.party && user.name) {
     connectedUsers.push(user)
@@ -27,8 +33,24 @@ function removeUser(socketId) {
   connectedUsers = connectedUsers.filter((user) => user.socketId !== socketId)
 }
 
-function emitConnectedList() {
+function emitConnectedList(socketId = null) {
+  console.log("connectedUsers", connectedUsers)
+  if (socketId) {
+    return io.to(socketId).emit("connected-list", connectedUsers)
+  }
+
   io.emit("connected-list", connectedUsers)
+}
+
+function updateChallenges(socketId) {
+  io.to(socketId).emit(
+    "challenges",
+    challengesList.filter((challenge) => {
+      console.log("challenge", challenge)
+      return challenge.userInvited.socketId === socketId
+    })
+  )
+  console.log("All challenges", challengesList)
 }
 
 io.on("connect", (socket) => {
@@ -44,19 +66,71 @@ io.on("connect", (socket) => {
     })
   })
 
-  socket.on("battle-invite", (socketInvited) => {
-    console.log("socketInvited", socketInvited)
+  socket.on("desconnect-server", (socketId) => {
+    removeUser(socketId)
+    console.log("Usuário desconectado:", socket.id)
+
+    emitConnectedList()
+  })
+
+  socket.on("connected-list", (socketId) => {
+    emitConnectedList(socketId)
+  })
+
+  socket.on("battle-invite", (socketInvitedId) => {
+    console.log("socketInvited", socketInvitedId)
     console.log("meu socket", socket.id)
 
-    // Enviar para o socket desafiado
+    challengesList = [
+      ...challengesList,
+      {
+        challengeId: crypto.randomUUID(),
+        challenger: getUserById(socket.id),
+        userInvited: getUserById(socketInvitedId),
+      },
+    ]
+    updateChallenges(socketInvitedId)
+  })
 
-    // Caso o invite esteja ok enviar para o socket desafiante que esta aguardando
+  socket.on("battle-invite-response", (challengeId, response) => {
+    const { userInvited, challenger } = challengesList.find(
+      (c) => c.challengeId === challengeId
+    )
 
-    // Em caso negativo enviar resposta para socket desafiante
+    // Aceitou e o desafiante esta disponível
+    if (response && !challenger.isInBattle) {
+      // Pode ser removido após impletação de tela de batalhas
+      challengesList = challengesList.filter(
+        (challenge) => challenge.challengeId !== challengeId
+      )
+      updateChallenges(userInvited.socketId)
+      updateChallenges(challenger.socketId)
+      ///////////////////////
+      connectedUsers = connectedUsers.map((user) => {
+        if (
+          user.socketId === userInvited.socketId ||
+          user.socketId === challenger.socketId
+        ) {
+          return { ...user, isInBattle: true }
+        }
+        return user
+      })
+      emitConnectedList()
+      io.to(userInvited.socketId).emit("battle", challenger)
+      io.to(challenger.socketId).emit("battle", userInvited)
+      return
+    }
 
-    // Em caso positivo enviar ambos para batalha
+    // Aceitou e o desafiante não esta disponível
+    if (response && getUserById(challengerSocketId).isInBattle) {
+      // Enviar um warning para o cliente que aceitou
+    }
 
-    // Atualizar algum status de inBattle
+    challengesList = challengesList.filter(
+      (challenge) => challenge.challengeId !== challengeId
+    )
+    updateChallenges(userInvited.socketId)
+    updateChallenges(challenger.socketId)
   })
 
   // Batalha
